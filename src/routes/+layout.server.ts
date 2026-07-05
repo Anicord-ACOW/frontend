aa25f1cae0accdffec11ad8dbfca5b9a2e5c4e01
agentflemme <agentflemme@protonmail.com>
2026-07-05 22:05:15 +0200

    imgonnakillthatfront

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,10 +3,11 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch, request }) => {
 	const apiUrl = env.API_ORIGIN || 'http://localhost:3000';
-	const cookieHeader = request.headers.get('cookie') || '';
+
 
 	let user = null;
 	try {
+		const cookieHeader = request.headers.get('cookie') || '';
 		const res = await fetch(`${apiUrl}/users/me`, {
             method: 'GET',
             headers: {
