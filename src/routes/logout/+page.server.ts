f613ac65f880b342d1b5fdd22e703002897caa00
agentflemme <agentflemme@protonmail.com>
2026-08-21 13:25:37 +0200

    trying to make this b work

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,7 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ cookies }) => {
-	cookies.delete('auth-token', { path: '/' });
+export const load: PageServerLoad = ({ url, cookies }) => {
+	let domain = undefined;
+	if (url.hostname.includes('cestpaspete.ovh')) {
+		domain = '.cestpaspete.ovh';
+	}
+	cookies.delete('auth-token', { path: '/', ...(domain && { domain }) });
 	redirect(302, '/');
 };
