5af9a2aed54c5330146840b2654cc1a81579d87a
agentflemme <agentflemme@protonmail.com>
2026-09-02 00:33:02 +0200

    feat: implement client-side handle hook to override URL with forwarded headers

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,1 +1,11 @@
 import './lib/client/init';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+    const proto = event.request.headers.get('x-forwarded-proto');
+    const host = event.request.headers.get('x-forwarded-host');
+    if (proto && host) {
+        event.url = new URL(`${proto}://${host}${event.url.pathname}${event.url.search}`);
+    }
+    return resolve(event);
+};
