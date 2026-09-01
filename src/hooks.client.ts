c77d45b39e8bd6aae62b25de230863162b879cdc
agentflemme <agentflemme@protonmail.com>
2026-09-02 00:45:54 +0200

    test

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,7 +1,16 @@
 import './lib/client/init';
+// src/hooks.server.ts
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+    console.log('=== DEBUG ===');
+    console.log('Origin:', event.request.headers.get('origin'));
+    console.log('Host:', event.request.headers.get('host'));
+    console.log('X-Forwarded-Host:', event.request.headers.get('x-forwarded-host'));
+    console.log('X-Forwarded-Proto:', event.request.headers.get('x-forwarded-proto'));
+    console.log('URL:', event.url.href);
+    console.log('=============');
+    
     const proto = event.request.headers.get('x-forwarded-proto');
     const host = event.request.headers.get('x-forwarded-host');
     if (proto && host) {
